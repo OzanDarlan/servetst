d3282f42072330077babe384dfef03f863e2f5be
Brad Montgomery <brad@bradmontgomery.net>
2019-09-27 11:48:47 -0500

    python3.7 updates and add argparse

--- dummy-web-server.py
+++ dummy-web-server.py
@@ -15,13 +15,14 @@ Send a POST request::
     curl -d "foo=bar&bin=baz" http://localhost
 
 """
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import SocketServer
+import argparse
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
 
 class S(BaseHTTPRequestHandler):
     def _set_headers(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header("Content-type", "text/html")
         self.end_headers()
 
     def do_GET(self):
@@ -30,22 +31,36 @@ class S(BaseHTTPRequestHandler):
 
     def do_HEAD(self):
         self._set_headers()
-        
+
     def do_POST(self):
         # Doesn't do anything with posted data
         self._set_headers()
         self.wfile.write("<html><body><h1>POST!</h1></body></html>")
-        
-def run(server_class=HTTPServer, handler_class=S, port=80):
-    server_address = ('', port)
+
+
+def run(server_class=HTTPServer, handler_class=S, addr="localhost", port=8000):
+    server_address = (addr, port)
     httpd = server_class(server_address, handler_class)
-    print 'Starting httpd...'
+
+    print(f"Starting httpd server on {addr}:{port}")
     httpd.serve_forever()
 
-if __name__ == "__main__":
-    from sys import argv
 
-    if len(argv) == 2:
-        run(port=int(argv[1]))
-    else:
-        run()
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Run a simple HTTP server")
+    parser.add_argument(
+        "-l",
+        "--listen",
+        default="localhost",
+        help="Specify the IP address on which the server listens",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=8000,
+        help="Specify the port on which the server listens",
+    )
+    args = parser.parse_args()
+    run(addr=args.listen, port=args.port)
