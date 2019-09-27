256b09ce3d6639c9fb325eaf92d79fb709f51e32
Brad Montgomery <brad@bradmontgomery.net>
2019-09-27 11:54:49 -0500

    extract html generation into a method

--- dummy-web-server.py
+++ dummy-web-server.py
@@ -30,9 +30,17 @@ class S(BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
+    def _html(self, message):
+        """This just generates an HTML document that includes `message`
+        in the body. Override, or re-write this do do more interesting stuff.
+
+        """
+        content = f"<html><body><h1>{message}</h1></body></html>"
+        return content.encode("utf8")  # NOTE: must return a bytes object!
+
     def do_GET(self):
         self._set_headers()
-        self.wfile.write(b"<html><body><h1>hi!</h1></body></html>")
+        self.wfile.write(self._html("hi!"))
 
     def do_HEAD(self):
         self._set_headers()
@@ -40,7 +48,7 @@ class S(BaseHTTPRequestHandler):
     def do_POST(self):
         # Doesn't do anything with posted data
         self._set_headers()
-        self.wfile.write(b"<html><body><h1>POST!</h1></body></html>")
+        self.wfile.write(self._html("POST!"))
 
 
 def run(server_class=HTTPServer, handler_class=S, addr="localhost", port=8000):
